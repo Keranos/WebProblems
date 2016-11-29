101a38776416040410dd4bc771849f5569c33437
Chris <christopherjamessmith@gmail.com>
2016-11-29 10:22:42 -0500

    Incorrect fib3 value

--- ProjectEuler/002.cpp
+++ ProjectEuler/002.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
 int main(){
-  long fib3 = 3;
+  long fib3 = 2;
   long fib6 = 0;
   long result = 2;
   long summed = 0;
 
-  while(result < 4000000){
+  while(result <= 4000000){
     summed += result;
     
     result = 4*fib3 + fib6;
